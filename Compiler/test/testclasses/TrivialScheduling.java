ac6d9432452f413397c6ba331bd271add04a9bda
chmaruni <christoph@mantuko.org>
2011-02-22 21:23:50 +0100

    added volatile array support for all types of arrays

--- Compiler/test/testclasses/TrivialScheduling.java
+++ Compiler/test/testclasses/TrivialScheduling.java
@@ -4,11 +4,27 @@ import scj.Task;
 
 public class TrivialScheduling {
 
-	private String[] names;
+	private boolean[] bools;
+	private byte[] bytes;
+	private char[] chars;
+	private double[] doubles;
+	private float[] floats;
+	private int[] ints;
+	private long[] longs;
+	private Object[] objects;
+	private short[] shorts;
 	
 	public void scjTask_subtask1(Task<Void> now) {
-		//System.out.println(names[0]);
-		String s = names[0];
+		System.out.println(bools[0] + ", " + bools[1]);
+		System.out.println(bytes[0] + ", " + bytes[1]);
+		System.out.println(chars[0] + ", " + chars[1]);
+		System.out.println(doubles[0] + ", " + doubles[1]);
+		System.out.println(floats[0] + ", " + floats[1]);
+		System.out.println(ints[0] + ", " + ints[1]);
+		System.out.println(longs[0] + ", " + longs[1]);
+		System.out.println(objects[0] + ", " + objects[1]);
+		System.out.println(shorts[0] + ", " + shorts[1]);
+		
 	}
 	
 //	public void scjTask_subtask2(Task<Void> now) {
@@ -18,9 +34,41 @@ public class TrivialScheduling {
 	public void scjMainTask_main(Task<Void> now) {
 		//System.out.println("main task");
 		
-		names = new String[2];
-		names[0] = "Subtask1";
-		//names[1] = "Subtask2";
+		bools = new boolean[2];
+		bools[0] = false;
+		bools[1] = true;
+		
+		bytes = new byte[2];
+		bytes[0] = 0;
+		bytes[1] = 1;
+		
+		chars = new char[2];
+		chars[0] = '0';
+		chars[1] = '1';
+		
+		doubles = new double[2];
+		doubles[0] = 0;
+		doubles[1] = 1;
+		
+		floats = new float[2];
+		floats[0] = 0;
+		floats[1] = 1;
+		
+		ints = new int[2];
+		ints[0] = 0;
+		ints[1] = 1;
+		
+		longs = new long[2];
+		longs[0] = 0;
+		longs[1] = 1;
+		
+		objects = new Object[2];
+		objects[0] = "0";
+		objects[1] = "1";
+		
+		shorts = new short[2];
+		shorts[0] = 0;
+		shorts[1] = 1;
 		
 		Task<Void> s1 = new Task<Void>();
 		this.scjTask_subtask1(s1);
--- Runtime/src/scj/Runtime.java
+++ Runtime/src/scj/Runtime.java
@@ -43,27 +43,136 @@ public final class Runtime {
     }
 	private static final Unsafe unsafe = getUnsafe();
 
-    private static final int base = unsafe.arrayBaseOffset(Object[].class);
-    private static final int scale = unsafe.arrayIndexScale(Object[].class);
+    private static final int byteBase = unsafe.arrayBaseOffset(byte[].class);
+    private static final int charBase = unsafe.arrayBaseOffset(char[].class);
+    private static final int doubleBase = unsafe.arrayBaseOffset(double[].class);
+    private static final int floatBase = unsafe.arrayBaseOffset(float[].class);
+    private static final int intBase = unsafe.arrayBaseOffset(int[].class);
+    private static final int longBase = unsafe.arrayBaseOffset(long[].class);
+    private static final int objectBase = unsafe.arrayBaseOffset(Object[].class);
+    private static final int shortBase = unsafe.arrayBaseOffset(short[].class);
     
-    private static long rawIndex(int i) {
+    private static final int byteScale = unsafe.arrayIndexScale(byte[].class);
+    private static final int charScale = unsafe.arrayIndexScale(char[].class);
+    private static final int doubleScale = unsafe.arrayIndexScale(double[].class);
+    private static final int floatScale = unsafe.arrayIndexScale(float[].class);
+    private static final int intScale = unsafe.arrayIndexScale(int[].class);
+    private static final int longScale = unsafe.arrayIndexScale(long[].class);
+    private static final int objectScale = unsafe.arrayIndexScale(Object[].class);
+    private static final int shortScale = unsafe.arrayIndexScale(short[].class);
+    
+    private static long rawIndex(int base, int scale, int i) {
         return base + (long) i * scale;
     }
-    
-	public static Object arrayReadObject(Object array, int i) {
-		if (i < 0 || i >= ((Object[])array).length)
-            throw new IndexOutOfBoundsException("index " + i);
+
+	public static byte arrayReadByteOrBoolean(Object array, int i) {
+		if(array instanceof byte[]) {
+			if(i < 0 || i >= ((byte[])array).length)
+				throw new IndexOutOfBoundsException("index " + i);
+		} else {
+			if(i < 0 || i >= ((boolean[])array).length)
+				throw new IndexOutOfBoundsException("index " + i);
+		}
 		
-		System.out.println("arrayReadObject: " + array + " i=" + i);
-		return unsafe.getObjectVolatile(array, rawIndex(i));
+		return unsafe.getByteVolatile(array, rawIndex(byteBase, byteScale, i));
 	}
-	
+	public static void arrayWriteByteOrBoolean(Object array, int i, byte newValue) {
+		if(array instanceof byte[]) {
+			if(i < 0 || i >= ((byte[])array).length)
+				throw new IndexOutOfBoundsException("index " + i);
+		} else {
+			if(i < 0 || i >= ((boolean[])array).length)
+				throw new IndexOutOfBoundsException("index " + i);
+		}
+			
+		unsafe.putByteVolatile(array, rawIndex(byteBase, byteScale, i), newValue);
+	}
+	public static char arrayReadChar(Object array, int i) {
+		if(i < 0 || i >= ((char[])array).length)
+			throw new IndexOutOfBoundsException("index " + i);
+			
+		return unsafe.getCharVolatile(array, rawIndex(charBase, charScale, i));
+	}
+	public static void arrayWriteChar(Object array, int i, char newValue) {
+		if(i < 0 || i >= ((char[])array).length)
+			throw new IndexOutOfBoundsException("index " + i);
+		unsafe.putCharVolatile(array, rawIndex(charBase, charScale, i), newValue);
+	}
+	public static double arrayReadDouble(Object array, int i) {
+		if(i < 0 || i >= ((double[])array).length)
+			throw new IndexOutOfBoundsException("index " + i);
+			
+		return unsafe.getDoubleVolatile(array, rawIndex(doubleBase, doubleScale, i));
+	}
+	public static void arrayWriteDouble(Object array, int i, double newValue) {
+		if(i < 0 || i >= ((double[])array).length)
+			throw new IndexOutOfBoundsException("index " + i);
+			
+		unsafe.putDoubleVolatile(array, rawIndex(doubleBase, doubleScale, i), newValue);
+	}
+	public static float arrayReadFloat(Object array, int i) {
+		if(i < 0 || i >= ((float[])array).length)
+			throw new IndexOutOfBoundsException("index " + i);
+			
+		return unsafe.getFloatVolatile(array, rawIndex(floatBase, floatScale, i));
+	}
+	public static void arrayWriteFloat(Object array, int i, float newValue) {
+		if(i < 0 || i >= ((float[])array).length)
+			throw new IndexOutOfBoundsException("index " + i);
+			
+		unsafe.putFloatVolatile(array, rawIndex(floatBase, floatScale, i), newValue);
+	}
+	public static int arrayReadInt(Object array, int i) {
+		if(i < 0 || i >= ((int[])array).length)
+			throw new IndexOutOfBoundsException("index " + i);
+			
+		return unsafe.getIntVolatile(array, rawIndex(intBase, intScale, i));
+	}
+	public static void arrayWriteInt(Object array, int i, int newValue) {
+		if(i < 0 || i >= ((int[])array).length)
+			throw new IndexOutOfBoundsException("index " + i);
+			
+		unsafe.putIntVolatile(array, rawIndex(intBase, intScale, i), newValue);
+	}
+	public static long arrayReadLong(Object array, int i) {
+		if(i < 0 || i >= ((long[])array).length)
+			throw new IndexOutOfBoundsException("index " + i);
+			
+		return unsafe.getLongVolatile(array, rawIndex(longBase, longScale, i));
+	}
+	public static void arrayWriteLong(Object array, int i, long newValue) {
+		if(i < 0 || i >= ((long[])array).length)
+			throw new IndexOutOfBoundsException("index " + i);
+			
+		unsafe.putLongVolatile(array, rawIndex(longBase, longScale, i), newValue);
+	}
+	public static Object arrayReadObject(Object array, int i) {
+		if(i < 0 || i >= ((Object[])array).length)
+			throw new IndexOutOfBoundsException("index " + i);
+			
+		return unsafe.getObjectVolatile(array, rawIndex(objectBase, objectScale, i));
+	}
 	public static void arrayWriteObject(Object array, int i, Object newValue) {
-		if (i < 0 || i >= ((Object[])array).length)
-            throw new IndexOutOfBoundsException("index " + i);
-		System.out.println("arrayWriteObject: " + array + " i=" + i + " val=" + newValue);
-		unsafe.putObjectVolatile(array, rawIndex(i), newValue);
+		if(i < 0 || i >= ((Object[])array).length)
+			throw new IndexOutOfBoundsException("index " + i);
+			
+		unsafe.putObjectVolatile(array, rawIndex(objectBase, objectScale, i), newValue);
 	}
+	public static short arrayReadShort(Object array, int i) {
+		if(i < 0 || i >= ((short[])array).length)
+			throw new IndexOutOfBoundsException("index " + i);
+			
+		return unsafe.getShortVolatile(array, rawIndex(shortBase, shortScale, i));
+	}
+	public static void arrayWriteShort(Object array, int i, short newValue) {
+		if(i < 0 || i >= ((short[])array).length)
+			throw new IndexOutOfBoundsException("index " + i);
+			
+		unsafe.putShortVolatile(array, rawIndex(shortBase, shortScale, i), newValue);
+	}
+
+
+
 	
 }
 
