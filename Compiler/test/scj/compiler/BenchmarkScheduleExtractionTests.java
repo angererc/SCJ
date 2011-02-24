e11d44c64f9ed263998be00a89350b42fcf6ecd7
chmaruni <christoph@mantuko.org>
2011-02-24 12:39:38 +0100

    added test for the task schedule extracted from barneshut

--- Compiler/test/scj/compiler/BenchmarkScheduleExtractionTests.java
+++ Compiler/test/scj/compiler/BenchmarkScheduleExtractionTests.java
@@ -1,5 +1,7 @@
 package scj.compiler;
 
+import static org.junit.Assert.*;
+
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
@@ -32,6 +34,30 @@ public class BenchmarkScheduleExtractionTests {
 		return new Compiler(options);
 	}
 	
+	private static class RelTester {
+		TaskSchedule<Integer, ?> schedule;
+		RelTester(TaskSchedule<Integer, ?> schedule) {
+			this.schedule = schedule;
+		}
+		
+		void assertHB(int a, int b) {
+			assertTrue(schedule.relationForNodes(a, b) == TaskSchedule.Relation.happensBefore);
+			assertTrue(schedule.relationForNodes(b, a) == TaskSchedule.Relation.happensAfter);
+		}
+		
+		void assertSingleton(int a) {
+			assertTrue(schedule.relationForNodes(a, a) == TaskSchedule.Relation.singleton);
+		}
+		
+		void assertOrdered(int a) {
+			assertTrue(schedule.relationForNodes(a, a) == TaskSchedule.Relation.ordered);
+		}
+		
+		void assertUnordered(int a) {
+			assertTrue(schedule.relationForNodes(a, a) == TaskSchedule.Relation.unordered);
+		}
+	}
+	
 	@Test
 	public void compileBarneshutDefault_ZeroXCFA() throws Exception {
 		Compiler compiler = compilerWithArgs(galoisBenchmarkArgs("barneshut", "default:ZeroXCFA"));
@@ -46,12 +72,62 @@ public class BenchmarkScheduleExtractionTests {
 		Set<IMethod> allTaskMethods = new HashSet<IMethod>();
 		allTaskMethods.addAll(mainTaskMethods);
 		allTaskMethods.addAll(taskMethods);
-		
+				
 		for(IMethod method : allTaskMethods) {
 			TaskSchedule<Integer, ?> schedule = driver.taskScheduleForTaskMethod(method);
+			RelTester t = new RelTester(schedule);
+			
 			System.out.println("=====================");
 			System.out.println(method.getName());
-			schedule.print(System.out);			
+			schedule.print(System.out);	
+			
+			String mName = method.getName().toString();
+			if(mName.equals("scjTask_ComputeTimeStep")) {
+				//ssa variables
+				int now = 2;
+				int later = 4;
+				int join = 35;
+				int compForce = 57;
+				int advance = 63;
+				//now hb all others (now hb later by transitivity) 
+				t.assertHB(now, later);
+				t.assertHB(now, join);
+				t.assertHB(now, compForce);
+				t.assertHB(now, advance);
+				
+				//all but compForce are singletons
+				t.assertSingleton(now);
+				t.assertSingleton(later);
+				t.assertSingleton(join);
+				t.assertSingleton(advance);
+				t.assertUnordered(compForce);
+				
+				//the real schedule we expect
+				t.assertHB(compForce, join);
+				t.assertHB(join, advance);
+				t.assertHB(advance, later);
+				
+			} else if (mName.equals("scjTask_process")) {
+				//only now
+				t.assertSingleton(2);
+				
+			} else if (mName.equals("scjMainTask_Main")) {
+				int now = 2;
+				int later = 4;
+				int iteration = 13;
+				
+				t.assertOrdered(iteration);
+				t.assertSingleton(later);
+				
+				t.assertHB(iteration, later);
+				t.assertHB(now, iteration);
+				t.assertHB(now, later);
+			} else if (mName.equals("scjTask_Advance")) {
+				//only now
+				t.assertSingleton(2);
+			} else {
+				assertTrue("unknown method: " + method.getName(), false);
+			}
 		}
 		
 	}
